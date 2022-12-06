aab93b54bb8689039554beaf51263e9e088546db
Safaroff90 <110519574+Safaroff90@users.noreply.github.com>
2022-12-07 03:26:47 +0400

    added Btn delete edit
    Delete edit btn added

---
+++ 301122/_Pustok/Models/Slider.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string? Title { get; set; }
+        [MaxLength(150)]
+        public string? Subtitle { get; set; }
+        [MaxLength(250)]
+        public string? Desc { get; set; }
+        [MaxLength(50)]
+        public string BtnText { get; set; }
+        [MaxLength(150)]
+        public string BtnUrl { get; set; }
+        [MaxLength(100)]
+        public string? Image { get; set; }
+        
+        public int Order { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+    }
+}
